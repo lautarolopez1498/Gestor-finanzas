a3bf37eba328de0864c955879cf4208f7a1e8ff9
lautarolopez1498 <82686600+lautarolopez1498@users.noreply.github.com>
2023-06-08 09:51:30 -0300

    Configurando react / webpack / babel

--- src/server.js
+++ src/server.js
@@ -30,7 +30,6 @@ server.listen(process.env.PORT, async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
     console.log('Conexión a la base de datos establecida correctamente');
   } catch (error) {
     console.error('Error al conectar a la base de datos:', error);
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app/index.js',
+  output: {
+    path: path.resolve( __dirname, './src/public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
